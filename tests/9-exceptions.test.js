fc46f23c4031bbc98e1aade17ac3a72d1fbb6a6a
ngimdock <ngimdockzemfack@gmail.com>
2023-08-05 13:03:57 +0100

    Test exceptions

--- tests/9-exceptions.test.js
+++ tests/9-exceptions.test.js
@@ -1,10 +1,13 @@
 function compileAndroidCode() {
-  throw new Error("And error occured..");
+  throw new Error("you are using the wrong JDK");
 }
 
 describe("Test exceptions", () => {
   test("compiling android goes as expected", () => {
     expect(() => compileAndroidCode()).toThrow();
     expect(() => compileAndroidCode()).toThrow(Error);
+
+    expect(() => compileAndroidCode()).toThrow("you are using the wrong JDK");
+    expect(() => compileAndroidCode()).toThrow(/JDK/);
   });
 });
